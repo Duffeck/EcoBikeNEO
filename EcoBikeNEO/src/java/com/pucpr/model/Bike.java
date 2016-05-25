67bb39749287f38a490c4cf629a2fef61fcd3d5b
lucas.duffeck <lucas.duffeck@LCET1124.cwbdis.pucpr.br>
2016-05-25 19:57:07 -0300

    Classe model!

---
+++ EcoBikeNEO/src/java/com/pucpr/model/Bike.java
@@ -0,0 +1,24 @@
+package com.pucpr.model;
+
+public class Bike {
+	private Integer capacidade;
+	private String numeroSerie;
+	
+	
+	public Integer getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(Integer capacidade) {
+		this.capacidade = capacidade;
+	}
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+	
+	
+	
+
+}
